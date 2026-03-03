cd03ed92d856145162d3fe0b5c6282eb6a0955e5
wai-lin <dev.wailin@gmail.com>
2026-03-03 20:58:29 +0700

    feat: add icon to board column title

--- src/features/boards/components/BoardColumn.tsx
+++ src/features/boards/components/BoardColumn.tsx
@@ -1,15 +1,35 @@
-import { Stack } from "#components"
+import { Icon, Stack } from "#components"
+import { Children } from "react"
 
 export interface BoardColumnProps {
 	title: string
+	iconColor?: string
 	children?: React.ReactNode
 }
 
-export function BoardColumn({ title, children }: BoardColumnProps) {
+export function BoardColumn({ title, iconColor, children }: BoardColumnProps) {
+	const hasChildren = Children.count(children) > 0
+
 	return (
 		<Stack direction="column" gap="md" className="board-column">
-			<h3 className="board-column-title">{title}</h3>
-			<Stack direction="column" gap="md" className="board-column-content">
+			<Stack direction="row" gap="xs" align="center">
+				<Icon
+					name="check-circle-solid"
+					style={{ color: iconColor, width: "1.2rem" }}
+				/>
+				<h3 className="board-column-title">{title}</h3>
+			</Stack>
+			<Stack
+				direction="column"
+				gap="md"
+				className="board-column-content"
+				style={{
+					// @ts-expect-error: Ignore the error! This is just changing css variable value
+					"--board-column-content-bg": hasChildren
+						? "transparent"
+						: "var(--gray-300)",
+				}}
+			>
 				{children}
 			</Stack>
 		</Stack>
--- src/features/boards/pages/Index.tsx
+++ src/features/boards/pages/Index.tsx
@@ -3,14 +3,26 @@ import { BoardColumn } from "../components/BoardColumn"
 
 export function Index() {
 	return (
-		<section>
+		<main className="container">
+			<header
+				style={{ paddingTop: "var(--sp-md)", marginBottom: "var(--sp-lg)" }}
+			>
+				<h3>Tasks</h3>
+			</header>
+
 			<Stack direction="row" gap="lg">
-				<BoardColumn title="Todo">
-					<p className="card">Define user requirements</p>
+				<BoardColumn title="Todo" iconColor="var(--color-primary)">
+					<h4 className="card">Authentication Flow</h4>
+					<h4 className="card">Create dashboard</h4>
 				</BoardColumn>
-				<BoardColumn title="InProgress"></BoardColumn>
-				<BoardColumn title="Done"></BoardColumn>
+				<BoardColumn title="InProgress" iconColor="var(--color-warning)">
+					<h4 className="card">Define user requirements</h4>
+				</BoardColumn>
+				<BoardColumn
+					title="Done"
+					iconColor="var(--color-success)"
+				></BoardColumn>
 			</Stack>
-		</section>
+		</main>
 	)
 }
