e25b010ba8d5c3d6aafaab643bed1e4e0e2126c6
wai-lin <dev.wailin@gmail.com>
2026-03-10 08:34:40 +0700

    build: use relative path in prod building

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from "vite"
 
 // https://vite.dev/config/
 export default defineConfig({
+	base: process.env.NODE_ENV === "production" ? "./" : "/",
 	plugins: [react()],
 	test: {
 		environment: "jsdom",
