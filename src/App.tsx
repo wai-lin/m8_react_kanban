50b6e28fb6266985ca5ac946d3568458a80e13ba
wai-lin <dev.wailin@gmail.com>
2026-03-10 08:30:01 +0700

    feat: add base url in router config

--- src/App.tsx
+++ src/App.tsx
@@ -3,30 +3,35 @@ import { RouterProvider } from "react-router/dom"
 import { AppLayout } from "./features/app_layouts"
 import { projectPages } from "./features/projects"
 
-const router = createBrowserRouter([
-	{
-		element: <AppLayout />,
-		children: [
-			{ index: true, Component: projectPages.Index },
-			{
-				path: "/:slug",
-				lazy: async () => {
-					const { boardsPages } = await import("./features/boards")
-					return { Component: boardsPages.Index }
-				},
-				children: [
-					{
-						path: ":taskId",
-						lazy: async () => {
-							const { tasksPages } = await import("./features/tasks")
-							return { Component: tasksPages.TaskDetail }
-						},
+const router = createBrowserRouter(
+	[
+		{
+			element: <AppLayout />,
+			children: [
+				{ index: true, Component: projectPages.Index },
+				{
+					path: "/:slug",
+					lazy: async () => {
+						const { boardsPages } = await import("./features/boards")
+						return { Component: boardsPages.Index }
 					},
-				],
-			},
-		],
+					children: [
+						{
+							path: ":taskId",
+							lazy: async () => {
+								const { tasksPages } = await import("./features/tasks")
+								return { Component: tasksPages.TaskDetail }
+							},
+						},
+					],
+				},
+			],
+		},
+	],
+	{
+		basename: import.meta.env.BASE_URL,
 	},
-])
+)
 
 export function App() {
 	return <RouterProvider router={router} />
